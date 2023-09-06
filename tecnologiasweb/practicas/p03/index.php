2566926b1f8b1d4aea90be78a3ce36ee6dfdc0b7
ItzD <itzeeldaniela@gmail.com>
2023-09-05 20:39:34 -0600

    Práctica 3. Ejercicio 7: Variables predefinidas.

--- tecnologiasweb/practicas/p03/index.php
+++ tecnologiasweb/practicas/p03/index.php
@@ -27,6 +27,7 @@
     echo '<li>$_elemnt1 es válida porque inicia $ y con guion bajo.</li>';
     echo '</ul>';
     ?>
+    <div style="width: 60%; text-align: justify">
     <h2>Ejercicio 2</h2>
     <p>2. Proporcionar los valores de $a, $b, $c como sigue:</p>
     <p>$a = “ManejadorSQL”;</p>
@@ -38,11 +39,11 @@
     $b = &$a;
     $c = &$a;
     echo '<p>a. Ahora muestra el contenido de cada variable</p>';
-    echo ($a);
+    echo "\$a = $a";
     echo '<br>';
-    echo ($b);
+    echo "\$b = $b";
     echo '<br>';
-    echo ($c);
+    echo "\$c = $c";
     ?>
     <p>b. Agrega al código actual las siguientes asignaciones:</p>
     <p>$a = “PHP server”;</p>
@@ -50,27 +51,29 @@
     <?php
       //Código PHP
     echo '<p>c. Vuelve a mostrar el contenido de cada uno:</p>';
-    echo ($a);
+    echo "\$a = $a";
     echo '<br>';
-    echo ($b);
+    echo "\$b = $b";
     echo '<br>';
+    echo "\$c = $c";
     ?>
     <p>d. Describe y muestra en la página obtenida qué ocurrió en el segundo bloque de
         asignaciones</p>
-    <ul>
-        <li>
-            Al modificar los valores de las variables $a y $b, los cambios afectan<br>
-            a los valores originales y a las variables que hacen referencia e él,<br>
-            porque no se realiza una copia de los valores. <br>
-            Es decir, que la variable contendra el último valor asignado, es por ello<br>
+        <strong>RESPUESTA. </strong>
+            Al modificar los valores de las variables $a y $b, los cambios afectan
+            a los valores originales y a las variables que hacen referencia e él,
+            porque no se realiza una copia de los valores. 
+            Es decir, que la variable contendra el último valor asignado, es por ello
             que al mostrar el contenido todo dice "PHP server".
-        </li>
-    </ul>
+        
+    </div>
+    
     <!--e. Utiliza la función correspondiente de PHP para liberar las variables utilizadas al final
     de cada ejercicio, de lo contrario habrá conflictos.-->
     <?php
     unset($a,$b,$c);
     ?>
+    <div style="width: 60%; text-align: justify">
     <h2>Ejercicio 3</h2>
     <p>3. Muestra el contenido de cada variable inmediatamente después de cada asignación, <br>
         verificar la evolución del tipo de estas variables (imprime todos los componentes de los
@@ -90,13 +93,13 @@
     $b = "5a versión de PHP";
     echo "Contenido de \$b después de la segunda asignación: $b<br>";
 
-    $c = $b * 10;
+    @$c = $b * 10;
     echo "Contenido de \$c después de la tercera asignación: $c<br>";
 
-    $a .= $b;
+    @$a .= $b;
     echo "Contenido de \$a después de la concatenación: $a<br>";
 
-    $b *= $c;
+    @$b *= $c;
     echo "Contenido de \$b después de la multiplicación: $b<br>";
 
     $z[0] = "MySQL";
@@ -104,6 +107,8 @@
     print_r($z);
     echo "<br>";
     ?>
+    </div>
+    
     <h2>Ejercicio 4</h2>
     <p>4. Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de <br>
         la matriz $GLOBALS o del modificador global de PHP.</p>
@@ -119,7 +124,8 @@
     echo "\$c tiene el valor de: $c";
     echo "<br>";
     $GLOBALS['z'];
-    echo "\$z tiene el valor de: $z";
+    echo "\$z tiene el valor de: ";
+    print_r($z);
     ?>
     <!--e. Utiliza la función correspondiente de PHP para liberar las variables utilizadas al final
     de cada ejercicio, de lo contrario habrá conflictos.-->
@@ -184,12 +190,28 @@
     $c_string = var_export($c_bool, true);
     $e_string = var_export($e_bool, true);
     
-    echo 'Función boolval() para obtener el valor booleano de la variable. <br>
+    echo '<strong>RESPUESTA. </strong>Función boolval() para obtener el valor booleano de la variable. <br>
         Después usar var_export() para convertir el valor booleano a una cadena.<br>
         Finalmente mostrarlo con echo.<br>';
     echo "\$c = $c_string";
     echo "<br>";
     echo "\$e = $e_string";
+    ?>
+
+    <h2>Ejercicio 7</h2>
+    <p>7. Usando la variable predefinida $_SERVER, determina lo siguiente:</p>
+    <?php
+    echo 'a. La versión de Apache y PHP<br>';
+    $apache_version = $_SERVER['SERVER_SOFTWARE'];
+    $php_version = phpversion();
+    echo "<strong>Versión de Apache:</strong> $apache_version<br>";
+    echo "<strong>Versión de PHP:</strong> $php_version<br>";
+
+    $server_os = $_SERVER['SERVER_NAME'];
+    echo "b. El nombre del sistema operativo (servidor):<strong> $server_os</strong><br>";
+    
+    $client_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    echo "c. El idioma del navegador (cliente): <strong>$client_language</strong><br>";
     ?>
 </body>
 
